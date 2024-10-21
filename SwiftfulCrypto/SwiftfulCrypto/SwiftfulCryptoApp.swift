80e089cbae36a1d26640fac496a6704844c30437
Shreya Pallan <shreyapallan@Shreyas-MBP.lan>
2024-10-21 13:18:39 +0530

    Remove Core Data files

--- SwiftfulCrypto/SwiftfulCrypto/SwiftfulCryptoApp.swift
+++ SwiftfulCrypto/SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -9,12 +9,10 @@ import SwiftUI
 
 @main
 struct SwiftfulCryptoApp: App {
-    let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
