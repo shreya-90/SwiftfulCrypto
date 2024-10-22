0a6659d229614fd5c8f9df3c84c0d85f36b1fcca
Shreya Pallan <shreyapallan@Shreyas-MBP.lan>
2024-10-22 14:46:59 +0530

    Adding CoinModel

--- SwiftfulCrypto/SwiftfulCrypto/Models/CoinModel.swift
+++ SwiftfulCrypto/SwiftfulCrypto/Models/CoinModel.swift
@@ -6,3 +6,112 @@
 //
 
 import Foundation
+
+
+//URL
+/*
+ URL: https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h
+ 
+ 
+ JSON Response:
+ 
+ {
+     "id": "bitcoin",
+     "symbol": "btc",
+     "name": "Bitcoin",
+     "image": "https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400",
+     "current_price": 67342,
+     "market_cap": 1331995460247,
+     "market_cap_rank": 1,
+     "fully_diluted_valuation": 1414774094635,
+     "total_volume": 41619944729,
+     "high_24h": 68597,
+     "low_24h": 66669,
+     "price_change_24h": -1147.79662081205,
+     "price_change_percentage_24h": -1.67587,
+     "market_cap_change_24h": -22913976241.0154,
+     "market_cap_change_percentage_24h": -1.69118,
+     "circulating_supply": 19771287,
+     "total_supply": 21000000,
+     "max_supply": 21000000,
+     "ath": 73738,
+     "ath_change_percentage": -8.45733,
+     "ath_date": "2024-03-14T07:10:36.635Z",
+     "atl": 67.81,
+     "atl_change_percentage": 99446.77814,
+     "atl_date": "2013-07-06T00:00:00.000Z",
+     "roi": null,
+     "last_updated": "2024-10-22T07:56:02.490Z",
+     "sparkline_in_7d": {
+       "price": [65294.8461405869, 65733.3633651136]
+     },
+     "price_change_percentage_24h_in_currency": -1.67586624598917
+   }
+ */
+
+// MARK: - Welcome
+struct CoinModel: Identifiable, Codable {
+    let id, symbol, name: String
+    let image: String
+    let currentPrice: Double
+    let marketCap, marketCapRank, fullyDilutedValuation: Double?
+    let totalVolume, high24H, low24H: Double?
+    let priceChange24H, priceChangePercentage24H, marketCapChange24H, marketCapChangePercentage24H: Double?
+    let circulatingSupply, totalSupply, maxSupply, ath: Double?
+    let athChangePercentage: Double?
+    let athDate: String?
+    let atl, atlChangePercentage: Double?
+    let atlDate: String?
+    let lastUpdated: String?
+    let sparklineIn7D: SparklineIn7D?
+    let priceChangePercentage24HInCurrency: Double?
+    
+    let currentHoldings: Double? // not from API
+    
+    
+    enum CodingKeys: String, CodingKey {
+        case id, symbol, name, image
+        case currentPrice = "current_price"
+        case marketCap = "market_cap"
+        case marketCapRank = "market_cap_rank"
+        case fullyDilutedValuation = "fully_diluted_valuation"
+        case totalVolume = "total_volume"
+        case high24H = "high_24h"
+        case low24H = "low_24h"
+        case priceChange24H = "price_change_24h"
+        case priceChangePercentage24H = "price_change_percentage_24h"
+        case marketCapChange24H = "market_cap_change_24h"
+        case marketCapChangePercentage24H = "market_cap_change_percentage_24h"
+        case circulatingSupply = "circulating_supply"
+        case totalSupply = "total_supply"
+        case maxSupply = "max_supply"
+        case ath
+        case athChangePercentage = "ath_change_percentage"
+        case athDate = "ath_date"
+        case atl
+        case atlChangePercentage = "atl_change_percentage"
+        case atlDate = "atl_date"
+        case lastUpdated = "last_updated"
+        case sparklineIn7D = "sparkline_in_7d"
+        case priceChangePercentage24HInCurrency = "price_change_percentage_24h_in_currency"
+        case currentHoldings
+    }
+    
+    func updateHoldings(amount: Double) -> CoinModel {
+        return CoinModel(id: id, symbol: symbol, name: name, image: image, currentPrice: currentPrice, marketCap: marketCap, marketCapRank: marketCapRank, fullyDilutedValuation: fullyDilutedValuation, totalVolume: totalVolume, high24H: high24H, low24H: low24H, priceChange24H: priceChange24H, priceChangePercentage24H: priceChangePercentage24H, marketCapChange24H: marketCapChange24H, marketCapChangePercentage24H: marketCapChangePercentage24H, circulatingSupply: circulatingSupply, totalSupply: totalSupply, maxSupply: maxSupply, ath: ath, athChangePercentage: athChangePercentage, athDate: athDate, atl: atl, atlChangePercentage: atlChangePercentage, atlDate: atlDate, lastUpdated: lastUpdated, sparklineIn7D: sparklineIn7D, priceChangePercentage24HInCurrency: priceChangePercentage24HInCurrency, currentHoldings: amount)
+    }
+    
+    var currentHoldingsValue: Double {
+        return (currentHoldings ?? 0) * currentPrice
+    }
+    
+    var rank: Int {
+        return Int(marketCapRank ?? 0)
+    }
+}
+
+
+// MARK: - SparklineIn7D
+struct SparklineIn7D: Codable {
+    let price: [Double]?
+}
